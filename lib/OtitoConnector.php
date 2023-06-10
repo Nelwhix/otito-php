894a987c7344e994db5dd0d7cbe6f94e13a0fb5a
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-06-10 07:48:08 +0100

    added connector

---
+++ lib/OtitoConnector.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OpenAPI\Client;
+
+use Sammyjo20\Saloon\Http\SaloonConnector;
+class OtitoConnector extends SaloonConnector
+{
+
+    public function defineBaseUrl(): string
+    {
+        return "https://api.otito.dev/v1";
+    }
+
+    public function defaultConfig(): array
+    {
+        return [
+            'timeout' => 60,
+        ];
+    }
+
+    public function defaultHeaders(): array
+    {
+        return [
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json'
+        ];
+    }
+
+    public function __construct(private string $apiKey){}
+
+}
